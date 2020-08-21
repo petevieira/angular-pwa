67d15d2647af4e6364f354be33bea7fe0616e67a
Peter Vieira <pete.vieira@languageconservancy.org>
2020-08-21 16:17:10 +0200

    updates

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,8 @@
 import { Component } from '@angular/core';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+import { Router } from "@angular/router";
+
+import { BooksService } from './books/books.service';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +10,21 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+  title = 'AngularBooksPWA';
+  searchForm: FormGroup;
+
+  constructor(private formBuilder: FormBuilder,
+              private router: Router) {
+  }
+
+  ngOnInit() {
+    this.searchForm = this.formBuilder.group({
+      search: ['', Validators.required],
+    });
+  }
+
+  onSearch() {
+    if (!this.searchForm.valid) return;
+    this.router.navigate(['search'], { queryParams: {query: this.searchForm.get('search').value}});
+  }
 }
